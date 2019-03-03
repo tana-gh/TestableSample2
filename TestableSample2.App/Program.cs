985786c505b9dc8517fd36a24e8ab5564963e8ba
tana <yozoranotakaku@gmail.com>
2019-03-03 11:07:36 +0900

    feat: プロジェクト構造

---
+++ TestableSample2.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestableSample2.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
